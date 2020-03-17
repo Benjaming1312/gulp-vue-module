87cec12866d0692c54d7d93c6d7518a5565076df
Ben <ashitman1312@gmail.com>
2020-03-17 23:32:18 +0800

    0317

--- gulp/js/userjs.js
+++ gulp/js/userjs.js
@@ -4,3 +4,77 @@
 // $('test').click((e) => {
 //     return e ? true : false
 // })
+function is (name) {
+  return $(name).is(name)
+}
+
+$('body').hide()
+setTimeout(() => {
+  $('body').fadeIn(300)
+}, 700);
+$(function () {
+  if (is('.idx.section-2')) {
+    $('.module-special').appendTo('.append-content')
+  }
+
+  function is (name) {
+    return $(name).is(name)
+  }
+
+  if (is('.module-viewcart')) {
+    let title = $('.module-viewcart .mt h3').text()
+    title = title.split('(')[0]
+    $('.module-viewcart .mt h3').text(title)
+  }
+  // if (!is('.idx.section')) {
+  //   $('.banner.idx').remove()
+  // }
+  // if ($(window).width() < 768 && is('.banner.img')) {
+  //   $('.banner.img .img').each(function (){
+  //     var src = $(this).find('img').attr('src')
+  //     $(this).next('.banner-title').attr('style', `background: url(${src})center center no-repeat`)
+  //   })
+  // }
+  // if (window.location.href.indexOf('new') >= 0) {
+  //   $('.banner.img.news-banner').removeClass('hidden')
+  //   $('.page.news.hidden').removeClass('hidden')
+  // }
+  // if (window.location.href.indexOf('prod') >= 0) {
+  //   $('.banner.img.case-banner').removeClass('hidden')
+  //   $('.page.case.hidden').removeClass('hidden')
+  // }
+  // if (window.location.href.indexOf('contact') >= 0) {
+  //   $('.banner.img.contact-banner').removeClass('hidden')
+  //   $('.page.contact.hidden').removeClass('hidden')
+  // }
+  // if (window.location.href.indexOf('newslist')) {
+  //   $('.blog').addClass('newslist')
+  // }
+
+  // if (window.location.href.indexOf('prod/') >= 0) {
+  //   $('.productlist-side').addClass('prod')
+  //   $('.productlist-main').addClass('prod')
+  // }
+
+  // if (window.location.href.indexOf('contact') >= 0) {
+  //   $('#wrap').addClass('contact')
+  //   $('#ContentPlaceHolder1_txtText').attr('placeholder', '您的LINE ID')
+  //   $('.contact.append').removeClass('hidden').prependTo('.contact.product .row .col-sm-8')
+  // }
+  // if ($(window).width() > 768) {
+  //   $('#ulMember2').appendTo($('#navbar'))
+  // }
+  // else {
+  //   $('#ulMember2').appendTo($('.navbar'))
+  // }
+
+  // if (is('.product-detail')) {
+  //   $('.productlist.container').addClass('prodetail')
+  // }
+  // if (is('.photoSlide-show')) {
+  //   let imgLink = $('.photoSlide-show').attr('style').split(`background-image: url('`)
+  //   imgLink = imgLink[1].split(`');`)[0]
+  //   $('.product-photoSlide').append(`<img class="img-responsive" src=${imgLink}>`)
+  // }
+  // $('#acart').appendTo($('body'))
+})
--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,7 @@ gulp.task('connect',function(){
     connect.server({
         name: "dist server",
         root: './',
-        port: 8001,
+        port: 8006,
         livereload: true
     })
 })
@@ -52,7 +52,8 @@ gulp.task('styles',function(){
             includeContent: false,
             sourceRoot: 'gulp/sass'
         })) //sourceMap寫入在一樣的地方  
-        .pipe(gulp.dest('dist/css')) //翻譯後的路徑
+        .pipe(gulp.dest('dist/css/hero129.so-buy.com/ezfiles/671/1671/img/8852/')) //翻譯後的路徑
+        .pipe(gulp.dest('dist/css/')) //翻譯後的路徑
         .pipe(connect.reload())
 })
 
